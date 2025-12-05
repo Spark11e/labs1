2f22eeb80136852d4d838e5d03a2d760b99da5a4
Spark11e <dimaandi804@gmail.com>
2025-12-05 23:42:19 +0400

    изменения стиля

--- upd hello_world.py
+++ upd hello_world.py
@@ -1,6 +1,9 @@
 import time
+
 def console_greeting():
+
   name = input("Введите ваше имя: ")
-  print("Hello world from {name}")
-  time.sleep(10)
+  print("Hello всем from {lastname}")
+  time.sleep(100)
+
 console_greeting()
